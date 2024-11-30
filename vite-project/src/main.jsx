6aadf50644bb8f0e4855bc8f87f2148947c3e0de
Kunal Dutta <dutta@DESKTOP-CJ5L034.(none)>
2024-11-30 17:23:53 +0530

    Started adding basic react setup

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,7 +1,18 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {Toaster} from 'react-hot-toast';
+
+import App from './App.jsx';
+import store from './Redux/Slices/store.js';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Provider store={store}>
+    <BrowserRouter>
     <App />
-)
+    <Toaster/>
+    </BrowserRouter> 
+    </Provider>
+);

20cf35625e322b1d8acf111d8829608681829015
Kunal Dutta <dutta@DESKTOP-CJ5L034.(none)>
2024-12-05 18:08:13 +0530

    Added basic layout and integrated to homepage

--- vite-project/src/Components/Footer.jsx
+++ vite-project/src/Components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
 return (
     <>
        <footer className='relative left-0 bottom-0 h-[10vh] py-5 flex flex-col sm:flex-row items-center justify-between text-white bg-gray-800 sm:px-20'>
-        <section className='text-lg'>
+        <section className='text-lg text-red'>
             Copyright {year} | All Rights Reserved.
         </section>
         <section className='flex items-center justify-center gap-5 text-2xl text-white'>
@@ -26,7 +26,7 @@ return (
 
        </footer>
     </>
-)
+);
 
 }
 
